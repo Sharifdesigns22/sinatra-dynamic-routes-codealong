105229652dba37453c40d58ba1815dac1e805e4e
Sharif Tarver <Shariftarver@gmail.com>
2021-03-18 07:51:20 -0700

    commit tree debd66f69173767807041819428b968cfde65c1d

--- app.rb
+++ app.rb
@@ -14,5 +14,21 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+
+  get '/goodbye' do
+    "Goodbye"
+  end
+
+  get "/multiply/:num1/:num2" do
+    @total = params[:num1].to_i * params[:num2].to_i 
+    @total.to_s
+    "#{@total}"
+  end 
+
+
 
 end
